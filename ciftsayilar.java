18d075ac43766dea0a5fa0a2adfcd669ad7e0c67
fur41 <115677824+fur41@users.noreply.github.com>
2022-11-15 23:29:45 +0300

    Add files via upload

---
+++ ciftsayilar.java
@@ -0,0 +1,18 @@
+﻿import java.util.Scanner;
+
+    public class ciftsayilar {
+    
+        public static void main(String[] args) {
+         Scanner inp =new Scanner(System.in);
+         
+         int sayı;
+	
+	System.out.print("Sayı giriniz :");		
+
+            sayı =inp.nextInt();
+       
+    for (int index = 0; index <=sayı ; index+=12) {System.out.println(index+",");
+        
+    }
+        }
+    }   
